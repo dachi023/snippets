c857d409103468c1c8cad9051dad87f348fcebd3
dachi <ar.19920223@gmail.com>
2016-02-14 06:49:30 +0900

    fixed #15 view snippets

---
+++ src/containers/Entry/index.js
@@ -0,0 +1,57 @@
+import React from 'react'
+import Firebase from 'firebase'
+import Previewer from '../../components/Previewer'
+import User from '../../store/User'
+
+class Entry extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      entry: {}
+    }
+  }
+
+  componentDidMount() {
+    const me = User.me()
+    if (!me) {
+      return this.context.router.push('/signup')
+    }
+    const path = `snippets/entries/${this.props.params.id}`
+    new Firebase(me.firebaseUrl).child(path).once('value', res => {
+      const entry = res.val()
+      if (!entry) {
+        return this.context.router.push('/signup')
+      }
+      this.setState({entry})
+    })
+  }
+
+  getStyles() {
+    return {
+      container: {
+        marginLeft: 'auto',
+        marginRight: 'auto'
+      },
+      content: {
+        marginLeft: '64px',
+        marginRight: '64px'
+      }
+    }
+  }
+
+  render() {
+    const styles = this.getStyles()
+    return (
+      <div style={styles.container}>
+        <div style={styles.content}>
+          <Previewer
+            title={this.state.entry.title}
+            content={this.state.entry.content}
+          />
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Entry
