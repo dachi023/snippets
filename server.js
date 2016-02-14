d87a5140a0e2237c6579fd301c5967518db3b0f2
dachi <ar.19920223@gmail.com>
2016-02-14 09:02:52 +0900

    update server.js

--- server.js
+++ server.js
@@ -1,17 +1,10 @@
 var express = require('express');
 var app = express();
 
-var user = process.env.USER;
-var pass = process.env.PASS;
-
 app.set('port', process.env.PORT || 3000);
 
-if (user && pass) {
-  app.use(express.basicAuth(user, pass));
-}
+app.use('/', express.static('./dist'));
 
-app.use(express.logger('dev'));
-app.use(express.compress());
-app.use(express.static(__dirname + '/dist'));
-
-app.listen(app.get('port'), function() {});
+app.listen(app.get('port'), function() {
+  console.log('server listening on port :' + app.get('port'));
+});
