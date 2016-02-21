f72c14ce860315afb03babc86ae4f89eba592be6
dachi <ar.19920223@gmail.com>
2016-02-21 22:00:00 +0900

    cosmetic change

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var app = express();
-var path = require('path')
+var path = require('path');
+
+var publicPath = path.join(__dirname, 'public');
 
 app.set('port', process.env.PORT || 3000);
 
-app.use('/bundle.js', express.static(path.join(__dirname, 'public', 'bundle.js')))
-
-app.use('/*', express.static(path.join(__dirname, 'public', 'index.html')));
+app.use('/bundle.js', express.static(path.join(publicPath, 'bundle.js')));
+app.use('/*', express.static(path.join(publicPath, 'index.html')));
 
 app.listen(app.get('port'), function() {
   console.log('server listening on port :' + app.get('port'));
