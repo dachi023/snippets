6dde5c4edb81fafe1b5cdcf3f241138f3dba60c7
dachi <ar.19920223@gmail.com>
2016-02-21 18:05:58 +0900

    cosmetic change

--- src/index.js
+++ src/index.js
@@ -1,28 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import {Router, Route, IndexRoute, browserHistory} from 'react-router'
+import {Router, browserHistory} from 'react-router'
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
-import App from './containers/App'
-import Signup from './containers/Signup'
-import Entries from './containers/Entries'
-import EntriesWip from './containers/EntriesWip'
-import EntryNew from './containers/EntryNew'
-import Entry from './containers/Entry'
-import EntryEdit from './containers/EntryEdit'
+import routes from './routes'
 
-ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/" component={App}>
-      <IndexRoute component={Signup} />
-      <Route path="signup" component={Signup} />
-      <Route path="settings" component="" />
-      <Route path="entries" component={Entries} />
-      <Route path="entries/wip" component={EntriesWip} />
-      <Route path="entry/new" component={EntryNew} />
-      <Route path="entry/:id" component={Entry}/>
-      <Route path="entry/:id/edit" component={EntryEdit} />
-    </Route>
-  </Router>
-), document.getElementById('root'))
+ReactDOM.render(<Router history={browserHistory} routes={routes} />, document.getElementById('root'))
